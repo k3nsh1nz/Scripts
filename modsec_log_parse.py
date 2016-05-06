6fcf329978b53b259f4ebe373fc3331df31a54ec
trieuvutrung <hokagevu173@gmail.com>
2016-05-06 12:16:09 +0700

    Update modsec_log_parse.py

--- modsec_log_parse.py
+++ modsec_log_parse.py
@@ -1,30 +1,37 @@
 #!/usr/bin/python
+#@: k
+
 import re
+import MySQLdb
 
-#k
+db = MySQLdb.connect("localhost","root","xxx","xxx")
+cursor = db.cursor()
 
-arr = []
 #parse log function
 def proccess_log(log):
-	date = re.findall(r'(\d.+\/\d+)\s(\d+:\d+:\d+)',log)
-	ip = re.findall(r'(\d+.\d+.\d+.\d+)]\s',log)
-	rules = re.findall(r'Pattern\smatch\s"(.+):"',log)
-	args = re.findall(r'at\s([a-zA-Z1-9:_-]+)',log)
-	id = re.findall(r'id\s"(\d+)"',log)
-	msg = re.findall(r'msg\s"([a-zA-Z1-9\s,]+)',log)
-	hostname = re.findall(r'\[hostname\s"([a-zA-Z1-9_-]+)',log)
-	uri = re.findall(r'uri\s"(\/[a-zA-Z1-9\.]+)',log)
-	arr.append(msg)
+	date = ' '.join(re.findall(r'(\d.+\/\d+)\s(\d+:\d+:\d+)',log)[0])
+	ip = ''.join(re.findall(r'(\d+.\d+.\d+.\d+)]\s',log)[0])
+	rules = ''.join(re.findall(r'Pattern\smatch\s"(.+):"',log)[0])
+	args = ''.join(re.findall(r'at\s([a-zA-Z1-9:_-]+)',log)[0])
+	rules_id = ''.join(re.findall(r'id\s"(\d+)"',log)[0])
+	msg = ''.join(re.findall(r'msg\s"([a-zA-Z1-9\s]+)',log)[0])
+	hostname = ''.join(re.findall(r'\[hostname\s"([a-zA-Z1-9_-]+)',log)[0])
+	uri = ''.join(re.findall(r'uri\s"(\/[a-zA-Z1-9\.]+)',log)[0])
+	
+	#insert log to mysql
+	cursor.execute("INSERT INTO log(date,ip,rules,args,rules_id,msg,hostname,uri) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)",(date,ip,rules,args,int(rules_id),msg,hostname,uri))
+	db.commit()
+
 #main
 def main():
-	file = open("/var/log/nginx/error.log","r")
+	file = open("modsec.log","r")
 	for line in file:
 		try:
 			proccess_log(line)
 		except Exception, e:
 			raise e
 
-	print arr
+	#print arr
 
 if __name__ == '__main__':
 	main()
