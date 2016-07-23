c23cc0020b3169785447d5275ad32df4ea67b7bc
trieuvutrung <hokagevu173@gmail.com>
2016-07-23 09:59:06 +0700

    IP Geolocation with nodejs

--- ipdb.js
+++ ipdb.js
@@ -3,7 +3,7 @@ var Curl = require( 'node-libcurl' ).Curl;
  
 var curl = new Curl();
  
-curl.setOpt( 'URL', 'ipinfo.io/117.4.250.39' );
+curl.setOpt( 'URL', 'ipinfo.io/8.8.8.8' );
 curl.setOpt( 'FOLLOWLOCATION', true );
  
 curl.on( 'end', function( statusCode, body, headers ) {
