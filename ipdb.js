337c606f432d11d628b6ff0df70d31dbe0b8a6ba
trieuvutrung <hokagevu173@gmail.com>
2016-07-23 09:57:14 +0700

    upload

---
+++ ipdb.js
@@ -0,0 +1,21 @@
+//npm install node-libcurl
+var Curl = require( 'node-libcurl' ).Curl;
+ 
+var curl = new Curl();
+ 
+curl.setOpt( 'URL', 'ipinfo.io/117.4.250.39' );
+curl.setOpt( 'FOLLOWLOCATION', true );
+ 
+curl.on( 'end', function( statusCode, body, headers ) {
+ 
+    console.info( statusCode );
+    console.info( '---' );
+    console.info(JSON.parse(body));
+    console.info( '---' );
+    console.info( this.getInfo( 'TOTAL_TIME' ) );
+ 
+    this.close();
+});
+ 
+curl.on( 'error', curl.close.bind( curl ) );
+curl.perform();
