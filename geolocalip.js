4235aae1f949e5d2ed5b6533b31bd21901f22152
kenshin17 <hokagevu173@gmail.com>
2016-07-25 15:23:37 +0700

    IP Geolocation with nodejs

--- geolocalip.js
+++ geolocalip.js
@@ -1,9 +1,14 @@
 //npm install node-libcurl
 var Curl = require( 'node-libcurl' ).Curl;
+var ip = process.argv[2];
  
 var curl = new Curl();
- 
-curl.setOpt( 'URL', 'ipinfo.io/8.8.8.8' );
+
+if (!ip) {
+	console.log("Usage: node "+process.argv[1]+" ip");
+	process.exit();
+}; 
+curl.setOpt( 'URL', 'ipinfo.io/'+ip );
 curl.setOpt( 'FOLLOWLOCATION', true );
  
 curl.on( 'end', function( statusCode, body, headers ) {
