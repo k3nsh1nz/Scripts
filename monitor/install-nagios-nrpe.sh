ce195ba2cb01b70189bd0e2daf32e8c5386fcbc2
kenshin17 <hokagevu173@gmail.com>
2016-08-31 11:43:16 +0700

    Create install-nagios-nrpe.sh

---
+++ monitor/install-nagios-nrpe.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+cd /usr/src
+wget https://www.nagios-plugins.org/download/nagios-plugins-1.5.tar.gz
+tar -xvf nagios-plugins-1.5.tar.gz
+cd nagios-plugins-1.5
+./configure
+make
+make install
+chown nagios.nagios /usr/local/nagios
+chown -R nagios.nagios /usr/local/nagios/libexec
+cd /usr/src
+wget http://liquidtelecom.dl.sourceforge.net/project/nagios/nrpe-2.x/nrpe-2.15/nrpe-2.15.tar.gz
+tar -xzf nrpe-2.15.tar.gz
+cd nrpe-2.15
+./configure
+make all
+make install-plugin
+make install-daemon
+make install-daemon-config
+make install-xinetd
+#echo 5666 /etc/services
+#service xinetd restart  
