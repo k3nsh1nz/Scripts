4dcee206621faf510b9b6d6f15753d33f117c9d5
trieuvt <trieuvutrung@vccorp.vn>
2017-11-02 11:57:29 +0700

    update solo user

---
+++ check_port_open.py
@@ -0,0 +1,18 @@
+import socket
+import sys
+ip = sys.argv[1]
+port = int(sys.argv[2])
+def check_port(ip,port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(2)
+    result = sock.connect_ex((ip,port))
+    if result == 0:
+        print "IP: %s | port %d OPEN" %(ip,port)
+    else:
+        print 'IP: %s | port %d CLOSED, connect_ex returned: %s' %(ip,port,str(result))
+
+def main():
+    check_port(ip,port)
+
+if __name__ == '__main__':
+    main()
